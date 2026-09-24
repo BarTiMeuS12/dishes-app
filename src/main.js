26178fabd55aae3344d8efb4bb8d1e7f26beee62
TimK <historytim2005@gmail.com>
2026-09-24 23:37:10 +0300

    form commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.delete-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+  });
+});
+
+document.getElementById('add-form').addEventListener('submit', e => {
+  e.preventDefault();
+});
